7a56e0126f5e19a3bb5db2d04345c9fa90f52a8a
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-09-07 11:55:03 -0400

    setup schema for books

---
+++ models/books.js
@@ -0,0 +1,37 @@
+module.exports = function (sequelize, DataTypes)
+{
+    var books = sequelize.define("books", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        isbn: {
+            type: DataTypes.CHAR,
+            allowNull: false
+            // validate: {
+            //     len: [20]
+            // }
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        condition: {
+            type: DataTypes.STRING
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0
+        },
+        comments: {
+            type: DataTypes.STRING
+        }
+    });
+
+    return books;
+};
+
